2f40b24c2eb8d4307a1d032d23a8f7c2a532cedc
jairusqt <jairuscasul04@gmail.com>
2022-06-10 23:16:21 +0800

    initial design

---
+++ src/components/searchWorkOrder.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container mx-auto">
+    <div class="grid grid-cols-6">
+        <div>
+            logo
+        </div>
+        <div class="col-span-4">
+            <input class="w-full h-10 rounded-3xl focus:outline-none p-2 border bg-emerald-200" type="text">
+        </div>
+        <div>
+            button
+        </div>
+    </div>
+</div>
+</template>
